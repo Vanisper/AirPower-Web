27aae9039db34ce590b2ebdbc522f39d66fe5a87
Hamm <majhamm@qq.com>
2025-04-02 15:16:58 +0800

    feat(hooks): 新增表格相关 Hook 函数
    - 添加 useBaseTable、useDetail、useEditor、useSelector、useTable、useTableTree 等 Hook 函数
    - 新增 WebI18n 类用于国际化支持
    - 更新 package.json 版本号至 0.0.25

---
+++ src/decorator/@table/TableType.ts
@@ -0,0 +1,3 @@
+export type TableFixed = 'left' | 'right'
+export type TableSortable = 'custom' | boolean
+export type TableAlign = 'left' | 'center' | 'right'
---
+++ src/decorator/@table/index.ts
@@ -0,0 +1,4 @@
+export * from './IElementTableColumn'
+export * from './ITableFieldConfig'
+export * from './Table'
+export * from './TableType'
