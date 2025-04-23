bf6883ba34285c6c1b9774c874c458db09db247f
Hamm <majhamm@qq.com>
2025-04-23 12:05:06 +0800

    refactor(table): 重构表格组件
    - 移除冗余代码和不必要的功能
    - 优化字段选择器逻辑
    - 调整样式和布局
    - 添加新的 ColumnSelector 组件

---
+++ src/components/input/DateTimeType.ts
@@ -0,0 +1,31 @@
+/**
+ * # 日期时间类型
+ *
+ * @author Hamm.cn
+ */
+export enum DateTimeType {
+  /**
+   * ### 日期
+   */
+  DATE = 'date',
+
+  /**
+   * ### 星期
+   */
+  WEEK = 'week',
+
+  /**
+   * ### 月份
+   */
+  MONTH = 'month',
+
+  /**
+   * ### 年份
+   */
+  YEAR = 'year',
+
+  /**
+   * ### 日期时间
+   */
+  DATETIME = 'datetime',
+}
