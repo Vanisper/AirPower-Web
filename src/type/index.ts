48edca5da7b0748c1c6b62e9fc9ad52b4e7571f9
Hamm <majhamm@qq.com>
2025-04-24 19:00:06 +0800

    refactor(project): 重构项目目录结构和代码组织
    - 更新目录结构：移动 curd 相关代码到更合理的路径
    - 重命名部分文件和目录，提高代码组织性
    - 更新导入路径，适应新的文件组织结构
    - 删除了一些未使用的接口和类

---
+++ src/type/index.ts
@@ -0,0 +1,25 @@
+/**
+ * ### 按钮图标
+ */
+export type ButtonIcon =
+  'ADD'
+  | 'EDIT'
+  | 'SEARCH'
+  | 'CLOCK'
+  | 'DELETE'
+  | 'LINK'
+  | 'LOCK'
+  | 'UNLOCK'
+  | 'CHECK'
+  | 'CLOSE'
+  | 'SETTING'
+  | 'WARNING'
+  | 'COPY'
+  | 'IMPORT'
+  | 'EXPORT'
+  | ''
+
+/**
+ * ### 金额方向
+ */
+export type MoneyDirection = 'up' | 'down' | 'round'
