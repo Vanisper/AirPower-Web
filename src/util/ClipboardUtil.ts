97c5275359acb5ff31f13039615e2ae6dcb647bd
Hamm <majhamm@qq.com>
2025-07-28 18:17:07 +0800

    fix(bugs): 修复了一些已知的BUG

---
+++ src/util/ClipboardUtil.ts
@@ -0,0 +1,17 @@
+import useClipboard from 'vue-clipboard3'
+
+/**
+ * ### 剪贴板工具类
+ *
+ * @author Hamm.cn
+ */
+export class ClipboardUtil {
+  /**
+   * ### 剪切板复制指定的内容
+   * @param content 复制的内容
+   */
+  static async copy(content: string): Promise<unknown> {
+    const { toClipboard } = useClipboard()
+    return toClipboard(content)
+  }
+}
